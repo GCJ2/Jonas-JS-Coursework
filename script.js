081c65b9075fc61158c7fb51a077a4b3434a124d
Greg Johnston <47114646+GCJ2@users.noreply.github.com>
2019-04-15 16:23:19 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,85 @@
+/*****************
+*Multiline comment
+******************/
+/*
+var firstName = 'John';
+console.log(firstName);
+
+var lastName = 'Smith';
+var age = 28;
+
+var fullAge = true;
+console.log(fullAge);
+
+var job = 'Teacher';
+console.log(job);
+
+//Variable naming rules
+var
+*/
+
+/*
+//Variable mutation and type coercion
+
+var firstName = 'John';
+var age = 28;
+
+console.log(firstName + ' ' + age);
+
+var job, isMarried;
+job = 'teacher';
+isMarried = false;
+
+console.log(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
+
+// Variable mutation
+
+age = 'twenty eight';
+job = 'driver';
+
+alert(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
+
+var lastName = prompt('What is his last name?');
+console.log(firstName + ' ' + lastName);
+*/
+
+/**************************************
+*Basic Operators
+*/
+/*
+var year, yearJohn, yearMark;
+now = 2018;
+ageJohn = 28;
+ageMark = 33;
+*/
+/*
+//Math Operators
+yearJohn = now - ageJohn;
+yearMark = now - ageMark;
+
+console.log(yearJohn);
+
+console.log(now + 2);
+console.log(now * 2);
+console.log(now / 10);
+
+//Logical Operators
+var johnOlder = ageJohn < ageMark;
+console.log(johnOlder);
+
+
+//typeof Operator
+console.log(typeof johnOlder);
+console.log(typeof ageJohn);
+console.log(typeof 'Mark is older than John');
+var x;
+console.log(typeof x);
+*/
+
+/*
+*Operator Precedence
+*/
+
+var now = 2018;
+var yearJohn = 1989;
+var fullAge =
